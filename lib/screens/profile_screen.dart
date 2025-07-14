20ebb4f393ffa863a4e70384457fcb202b7fee1a
ArmaanGilani <armangilani777@gmail.com>
2025-07-14 08:53:08 +0530

    Updated Code

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -82,7 +82,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
 
-                      Text("Stefani Wong1",
+                      Text("Stefani Wong",
                         style: GoogleFonts.poppins(
                           textStyle: TextStyle(
                               fontSize: 20,
