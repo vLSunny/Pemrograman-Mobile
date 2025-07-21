2427aaa186d7c81f20d0464fb13110e9e892bb54
ArmaanGilani <armangilani777@gmail.com>
2025-07-21 23:20:40 +0530

    Updated Code

---
+++ lib/screens/goal_screen3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoalScreen3 extends StatefulWidget {
+  const GoalScreen3({super.key});
+
+  @override
+  State<GoalScreen3> createState() => _GoalScreen3State();
+}
+
+class _GoalScreen3State extends State<GoalScreen3> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
