f191033a6149444616c78bcc34de05f7261f77de
ArmaanGilani <armangilani777@gmail.com>
2025-07-22 22:37:51 +0530

    Updated Code

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -447,6 +447,84 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                 ),
               ),
 
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Expanded(
+                      child: Row(
+                        children: [
+
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: Color(0XFFF5E9FC),
+                              borderRadius: BorderRadius.circular(30), // Rounded corners
+                            ),
+                            child: Icon(
+                              Icons.sports_gymnastics_rounded,
+                              size: 30.0,
+                            ),
+                          ),
+
+                          SizedBox(width: 10.0),
+
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+
+                                Text("Ups, You have missed your Lowerbody workout",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black
+                                    ),
+                                  ),
+                                ),
+
+                                Text("3 April",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey
+                                    ),
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+
+                    Icon(
+                        Icons.more_vert_rounded,
+                        size: 30.0,
+                        color: Colors.grey
+                    ),
+
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Divider(
+                  color: Colors.grey,
+                  thickness: 1.0,
+                  indent: 20.0,
+                  endIndent: 20.0,
+                ),
+              ),
+
             ],
           ),
       ),
