e05f6b3fdecb50344a6991229a40aa3c20dea5f0
ArmaanGilani <armangilani777@gmail.com>
2025-07-22 21:53:42 +0530

    Updated Code

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -13,33 +13,29 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(),
-
-      /*appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.white,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Welcome Back,",
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black,
-                ),
-              ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Notification",
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black
             ),
-            Text("Stefani Wong",
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black
-                ),
-              ),
+          ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 0.0, 0.0, 10.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(12),
             ),
-          ],
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black,),
+            ),
+          ),
         ),
         actions: [
           Padding(
@@ -51,13 +47,12 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
               ),
               child: IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.notification_important, color: Colors.black,),
+                icon: Icon(Icons.more_horiz_rounded, color: Colors.black,),
               ),
             ),
           ),
         ],
-
-      ),*/
+      ),
 
       body: Center(
         child: Text("NotificationsScreen!!!"),
