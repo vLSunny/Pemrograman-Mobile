c10c4768e10328b723bcf8bd629739df58f986f0
ArmaanGilani <armangilani777@gmail.com>
2025-07-22 22:05:46 +0530

    Updated Code

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -54,8 +54,81 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
         ],
       ),
 
-      body: Center(
-        child: Text("NotificationsScreen!!!"),
+      body: SafeArea(
+          child: Column(
+            children: [
+
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Row(
+                      children: [
+
+                        Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: Color(0XFFECF1FD),
+                            borderRadius: BorderRadius.circular(30), // Rounded corners
+                          ),
+                          child: Icon(
+                            Icons.food_bank_outlined,
+                            size: 30.0,
+                          ),
+                        ),
+
+                        Padding(
+                          padding: EdgeInsets.only(left: 10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+
+                              Text("Hey, it's time for lunch",
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black
+                                  ),
+                                ),
+                              ),
+
+                              Text("About 1 minute ago",
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey
+                                  ),
+                                ),
+                              ),
+
+                            ],
+                          ),
+                        ),
+
+                      ],
+                    ),
+
+                    Icon(
+                      Icons.more_vert_rounded,
+                      size: 30.0,
+                      color: Colors.grey
+                    ),
+
+                  ],
+                ),
+              ),
+
+              Divider(
+
+              ),
+
+            ],
+          ),
       ),
 
     );
