d91c0ed58413215f67c7fc68d65d80696cf35021
ArmaanGilani <armangilani777@gmail.com>
2025-07-22 22:34:43 +0530

    Updated Code

--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -305,7 +305,7 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                             width: 60,
                             height: 60,
                             decoration: BoxDecoration(
-                              color: Color(0XFFECF0FF),
+                              color: Color(0XFFF5E9FC),
                               borderRadius: BorderRadius.circular(30), // Rounded corners
                             ),
                             child: Icon(
@@ -332,6 +332,84 @@ class _NotificationsScreenState extends State<NotificationsScreen> {
                                 ),
 
                                 Text("29 May",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey
+                                    ),
+                                  ),
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+
+                    Icon(
+                        Icons.more_vert_rounded,
+                        size: 30.0,
+                        color: Colors.grey
+                    ),
+
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Divider(
+                  color: Colors.grey,
+                  thickness: 1.0,
+                  indent: 20.0,
+                  endIndent: 20.0,
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Expanded(
+                      child: Row(
+                        children: [
+
+                          Container(
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: Color(0XFFE9F0FF),
+                              borderRadius: BorderRadius.circular(30), // Rounded corners
+                            ),
+                            child: Icon(
+                              Icons.lunch_dining,
+                              size: 30.0,
+                            ),
+                          ),
+
+                          SizedBox(width: 10.0),
+
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+
+                                Text("Hey, it's time for lunch",
+                                  style: GoogleFonts.poppins(
+                                    textStyle: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black
+                                    ),
+                                  ),
+                                ),
+
+                                Text("8 April",
                                   style: GoogleFonts.poppins(
                                     textStyle: TextStyle(
                                         fontSize: 12,
