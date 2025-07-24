41540ae23004c4a7c453c09f2ce63553f7af3f3e
ArmaanGilani <armangilani777@gmail.com>
2025-07-24 16:27:03 +0530

    Updated Code

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key});
@@ -12,8 +13,57 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Center(
-        child: Text("ScheduleScreen"),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Workout Schedule",
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black
+            ),
+          ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 0.0, 0.0, 10.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.black,),
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_horiz_rounded, color: Colors.black,),
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+
+                Text("data"),
+
+              ],
+            ),
+          ),
       ),
 
     );
