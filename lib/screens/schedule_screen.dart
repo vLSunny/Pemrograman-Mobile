dc332372b7aae2795d948db34b5ab9a4e436cbcf
ArmaanGilani <armangilani777@gmail.com>
2025-07-24 16:51:51 +0530

    Updated Code

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class ScheduleScreen extends StatefulWidget {
   const ScheduleScreen({super.key});
@@ -52,20 +53,102 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
             ),
           ),
         ],
+        backgroundColor: Colors.white,
       ),
 
       body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
+          child: Column(
+            children: [
 
-                Text("data"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
 
-              ],
-            ),
+                  Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.grey,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                      child:                               Text("About 1 minute ago",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey
+                          ),
+                        ),
+                      ),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios_outlined,
+                    color: Colors.grey,
+                  ),
+
+                ],
+              ),
+
+              Expanded(
+                child: SfCalendar(
+                  view: CalendarView.day,
+                  dataSource: WorkoutDataSource(_getWorkoutData()),
+                  timeSlotViewSettings: TimeSlotViewSettings(
+                    startHour: 6,
+                    endHour: 20,
+                    timeFormat: 'h:mm a',
+                  ),
+                  todayHighlightColor: Colors.deepPurple,
+                  appointmentTextStyle: TextStyle(color: Colors.white),
+
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.only(top: 60.0),
+                child: Text("data"),
+              ),
+
+            ],
           ),
       ),
 
+      backgroundColor: Colors.white,
+
     );
+  }
+
+  List<Appointment> _getWorkoutData() {
+    return <Appointment>[
+      Appointment(
+        startTime: DateTime.now().add(Duration(hours: 1)),
+        endTime: DateTime.now().add(Duration(hours: 2)),
+        subject: 'Ab Workout',
+        color: Colors.pinkAccent,
+        startTimeZone: '',
+        endTimeZone: '',
+      ),
+      Appointment(
+        startTime: DateTime.now().add(Duration(hours: 3)),
+        endTime: DateTime.now().add(Duration(hours: 4)),
+        subject: 'Upperbody Workout',
+        color: Colors.purpleAccent,
+        startTimeZone: '',
+        endTimeZone: '',
+      ),
+      Appointment(
+        startTime: DateTime.now().add(Duration(hours: 9)),
+        endTime: DateTime.now().add(Duration(hours: 10)),
+        subject: 'Lowerbody Workout',
+        color: Colors.deepPurple.shade200,
+        startTimeZone: '',
+        endTimeZone: '',
+      ),
+    ];
+  }
+}
+
+class WorkoutDataSource extends CalendarDataSource {
+  WorkoutDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
